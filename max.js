6def84df16add3d93db0dff11a46fdca4e7b2830
Raj_Dash <rajdash3472@gmail.com>
2022-08-09 12:51:34 +0600

    max.js file added

---
+++ max.js
@@ -0,0 +1,12 @@
+const jim = 69;
+const dela = 97;
+
+const maxNumber = Math.max(69, 97);
+// console.log(maxNumber);
+
+// if (jim > dela) {
+//     console.log("Jim is a good boy!");
+// }
+// else {
+//     console.log("Dela is a good girl")
+// }
