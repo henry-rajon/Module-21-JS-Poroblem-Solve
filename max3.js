a83a9b0d5bf2ac7bd45a360869dbad240b1c3089
Raj_Dash <rajdash3472@gmail.com>
2022-08-09 12:59:14 +0600

    max3.js file added

---
+++ max3.js
@@ -0,0 +1,16 @@
+const jim = 69;
+const dela = 97;
+const chinku = 99;
+
+const maxNumber = Math.max(69, 97, 99);
+// console.log(maxNumber);
+
+if (jim > dela && jim > chinku) {
+    console.log("Jim is the first boy in the class");
+}
+else if (dela > jim && dela > chinku) {
+    console.log("Dela is the first girl in the class");
+}
+else {
+    console.log("Chinku is the first boy in the class");
+}
